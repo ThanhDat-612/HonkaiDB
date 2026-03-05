b1f563cd3cfebff28071c56ad821856a90f7c3e6
ThanhDat-612 <datviproo6a5@gmail.com>
2026-03-05 23:51:22 +0700

    update  filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.css";
 import { connectDB } from "@/lib/mongodb";
 import Character from "@/models/Character";
+import CharacterList from "@/components/CharacterList";
 
 export const metadata = {
   title: "Honkai Starrail DB",
@@ -8,8 +9,8 @@ export const metadata = {
 export default async function Home() {
 
   await connectDB();
-  const characters = await Character.find({});
-  console.log(characters);
+  const rawCharacters = await Character.find({});
+  const characters = JSON.parse(JSON.stringify(rawCharacters));
   return (
     <div className={styles.parent}>
       {/* NAVBAR */}
@@ -30,64 +31,8 @@ export default async function Home() {
       </div>
 
       <div className={styles.div2}>Sidebar</div>
-      <div className={styles.div3}>
-        {/* FILTER */}
-        <div className={styles.filterSection}>
-          <select>
-            <option>All Elements</option>
-            <option>Fire</option>
-            <option>Ice</option>
-            <option>Lightning</option>
-            <option>Wind</option>
-            <option>Quantum</option>
-            <option>Imaginary</option>
-          </select>
-
-          <select>
-            <option>All Paths</option>
-            <option>Destruction</option>
-            <option>Hunt</option>
-            <option>Erudition</option>
-            <option>Harmony</option>
-            <option>Nihility</option>
-            <option>Preservation</option>
-            <option>Abundance</option>
-          </select>
-        </div>
-        {/* CHARACTER GRID */}
-          <div className={styles.characterGrid}>
-            {characters.map((c) => (
-              <div key={c._id} className={styles.card}>
-                
-                <div className={styles.cardImage}>
-                  <img src={c.image} alt={c.name}/>
-                </div>
-
-                <div className={styles.cardInfo}>
-                  <h3>{c.name}</h3>
-
-                  <p className={styles.rarity}>
-                    {"⭐".repeat(c.rarity)}
-                  </p>
-
-                  <div className={styles.iconRow}>
-                    <img 
-                        src={`/elements/${c.element.toLowerCase()}.png`} 
-                        alt={c.element}
-                        className={styles.icon}
-                    />
-                    <img 
-                        src={`/paths/${c.path}.png`} 
-                        alt={c.path}
-                        className={styles.icon}
-                    />
-                  </div>
-                </div>
-
-              </div>
-            ))}
-          </div>
-      </div>
+      <CharacterList characters={characters} />
+      
     </div>
   );
 }
